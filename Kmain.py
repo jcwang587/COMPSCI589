91e6b0d8b5d69dec422949fe6ca62bdc430b053b
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-02-08 01:48:12 -0500

    update

--- Kmain.py
+++ Kmain.py
@@ -1,16 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from collections import Counter
-
-
-def euclidean_distance(data_1, data_2, data_len):
-    dist = 0
-    for i in range(data_len):
-        dist = dist + np.square(data_1[i] - data_2[i])
-    return np.sqrt(dist)
-
 
 # Load the dataset
 df = pd.read_csv('iris.csv', header=None)
@@ -28,15 +21,25 @@ X = (X - X.min()) / (X.max() - X.min())
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train the k-NN algorithm
-correct = 0
-for index, row in X_train.iterrows():
-    print(y_train[index])
-    x = row.values
-    distance = [np.sqrt(np.sum((row.values - x) ** 2)) for index2, row in X_train.iterrows()]
-    idx_sort = np.argsort(distance)
-    y_idx_sort = y_train.values[idx_sort]
-    topK_y = y_idx_sort[:1]
-    c = Counter(topK_y)
-    print(c.most_common()[0][0])
-    if c.most_common()[0][0] == y_train[index]:
-        correct += 1
+accuracy = []
+for k in range(1, 120, 2):
+    correct = 0
+    for index, row in X_train.iterrows():
+        print(y_train[index])
+        x = row.values
+        distance = [np.sqrt(np.sum((row.values - x) ** 2)) for index2, row in X_train.iterrows()]
+        idx_sort = np.argsort(distance)
+        y_idx_sort = y_train.values[idx_sort]
+        topK_y = y_idx_sort[:k]
+        c = Counter(topK_y)
+        print(c.most_common()[0][0])
+        if c.most_common()[0][0] == y_train[index]:
+            correct += 1
+    accuracy.append(correct / 120)
+l1 = plt.plot(range(1, 120, 2), accuracy, 'b-', label='type1')
+plt.plot(range(1, 120, 2), accuracy, 'bo-')
+plt.title('The Lasers in Three Conditions')
+plt.xlabel('row')
+plt.ylabel('column')
+plt.legend()
+plt.show()
