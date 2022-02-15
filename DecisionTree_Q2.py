68a7733c07eea084b1620a195cf98c0f16999442
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-02-14 22:45:09 -0500

    update

--- DecisionTree_Q2.py
+++ DecisionTree_Q2.py
@@ -10,83 +10,76 @@ def split_data(data_input, split_index, value):
     data_output = []
     for data in data_input:
         if data[split_index] == value:
-            subData = data[:split_index]
-            subData.extend(data[split_index + 1:])
-            data_output.append(subData)
+            new_data = data[:split_index]
+            new_data.extend(data[split_index + 1:])
+            data_output.append(new_data)
     return data_output
 
 
 def calculate_entropy(data_input):
-    numEntries = len(data_input)  # 数据集长度
-    classList = [data[-1] for data in data_input]  # 数据集的类别集合
+    label_list = [data[-1] for data in data_input]
     classCount = {}  # 类别计数
-    shannonEnt = 0.0  # 信息嫡
-    # 遍历计数
-    for value in classList:
+    entropy = 0
+    for value in label_list:
         if value not in classCount.keys():
             classCount[value] = 0
         classCount[value] += 1
-
-    # 计算信息嫡
     for key in classCount.keys():
-        prob = float(classCount[key]) / numEntries
-        shannonEnt -= prob * math.log(prob, 2)
-    return shannonEnt
+        prob = float(classCount[key]) / len(data_input)
+        entropy -= prob * math.log(prob, 2)
+    return entropy
 
 
-def chooseBestFeature(dataSet):
-    numEntries = len(dataSet)  # 数据集数量
-    numFeatures = len(dataSet[0]) - 1  # 属性的数量
-    baseEnt = calculate_entropy(dataSet)  # 整个数据集的香农嫡
-    bestFeature = -1  # 选出的最好属性的index
-    maxGain = 0.0  # 记录最大的信息增益
-    for i in range(numFeatures):
+def compare_information_gain(data_input):
+    branch_index = -1  # 选出的最好属性的index
+    information_gain = 0.0  # 记录最大的信息增益
+    for i in range(len(data_input[0]) - 1):
         newEnt = 0.0  # 划分之后的信息嫡之和
-        featList = [data[i] for data in dataSet]  # 获取所有该属性的所有值
+        featList = [data[i] for data in data_input]  # 获取所有该属性的所有值
         featSet = set(featList)  # 获取该属性不同的值
         for value in featSet:
-            subDataSet = split_data(dataSet, i, value)  # 获取属性值相同的数据集
-            prob = float(len(subDataSet)) / numEntries
+            subDataSet = split_data(data_input, i, value)  # 获取属性值相同的数据集
+            prob = float(len(subDataSet)) / len(data_input)
             newEnt += prob * calculate_entropy(subDataSet)
-        newGain = baseEnt - newEnt
-        if newGain > maxGain:
-            maxGain = newGain
-            bestFeature = i
-    return bestFeature
+        # Calculate information gain
+        newGain = calculate_entropy(data_input) - newEnt
+        if newGain > information_gain:
+            information_gain = newGain
+            branch_index = i
+    return branch_index
 
 
 def create_decision_tree(data_input, attribute):
     classList = [data[-1] for data in data_input]  # 获取数据集中所属类别的数据
-    # 检测数据集是否符合同一个分类，相同则返回
+    # If all instances belong to the same class
     if classList.count(classList[0]) == len(classList):
         return classList[0]
-    # 假如属性用完了，那么选择当前数据集中数量最多的类别
+    # If there are no more attributes that can be tested
     if len(data_input[0]) == 1:
         return max(classList, key=classList.count)
     # 选取最好的属性，采取ID3
-    bestFeature = chooseBestFeature(data_input)  # 最好属性的index
-    bestFeatLabel = attribute[bestFeature]  # 最好属性的名字
-    decisionTree = {bestFeatLabel: {}}  # 存储决策树
-    featValues = [data[bestFeature] for data in data_input]
+    branch_index = compare_information_gain(data_input)  # 最好属性的index
+    branch = attribute[branch_index]  # 最好属性的名字
+    decision_tree = {branch: {}}  # 存储决策树
+    featValues = [data[branch_index] for data in data_input]
     featValuesSet = set(featValues)  # 不同类别的集合
-    del (attribute[bestFeature])
+    del (attribute[branch_index])
     for value in featValuesSet:
-        subLabels = attribute[:]  # 针对划分之后的数据集都要有一个新的labels
-        decisionTree[bestFeatLabel][value] = create_decision_tree(split_data(data_input, bestFeature, value), subLabels)
-    return decisionTree
+        new_label = attribute[:]
+        decision_tree[branch][value] = create_decision_tree(split_data(data_input, branch_index, value), new_label)
+    return decision_tree
 
 
-def predict(tree, labels, testData):
-    firstLabel = list(tree.keys())[0]
-    firstLabelIndex = labels.index(firstLabel)
-    secondDict = tree[firstLabel]
-    value = testData[firstLabelIndex]
-    classLabel = None
-    if type(secondDict[value]).__name__ == "dict":
-        classLabel = predict(secondDict[value], labels, testData)
+def predict(tree, attribute_list, test_data):
+    label = list(tree.keys())[0]
+    label_index = attribute_list.index(label)
+    dictionary = tree[label]
+    value = test_data[label_index]
+    if type(dictionary[value]).__name__ == "dict":
+        prediction = predict(dictionary[value], attribute_list, test_data)
     else:
-        classLabel = secondDict[value]
-    return classLabel
+        prediction = dictionary[value]
+    return prediction
 
 
 if __name__ == '__main__':
@@ -99,18 +92,15 @@ if __name__ == '__main__':
         try:
             # Shuffle the dataset
             df_sf = shuffle(df)
-            X = df_sf[df.columns[0:17]]
             y = df_sf[df.columns[16]]
 
             # Randomly partition the dataset
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+            data_train, data_test, y_train, y_test = train_test_split(df_sf, y, test_size=0.2)
 
             # Normalize the dataset
-            X_train = (X_train - X_train.min()) / (X_train.max() - X_train.min())
-            X_test = (X_test - X_test.min()) / (X_test.max() - X_test.min())
-            X_train_data_list = X_train.values.tolist()
-            X_test_data_list = X_test.values.tolist()
-            X_train_attribute_list = X_train.keys().to_list()
+            X_train_data_list = data_train.values.tolist()
+            X_test_data_list = data_test.values.tolist()
+            X_train_attribute_list = data_train.keys().to_list()
 
             # Create decision tree
             X_train_attribute_list_copy = X_train_attribute_list[:]
