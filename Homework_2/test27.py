04ccb367549d54d5834141620e95cd130af13598
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-02-27 13:18:25 -0500

    update

--- Homework_2/test27.py
+++ Homework_2/test27.py
@@ -1,21 +1,16 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from collections import Counter
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
-from utils import *
-import pprint
 
 
-def words2vec(vocab_list, input_set):
-    return_vec = [0] * len(vocab_list)
-    for words in input_set:
-        if words in vocab_list:
-            return_vec[vocab_list.index(words)] = 1
-        else:
-            print("the word: %s is not in my Vocabulary!" % words)
-    return return_vec
+def get_multi_data():
+    x = np.array([
+        [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3],
+        [4, 5, 5, 4, 4, 4, 5, 5, 6, 6, 6, 5, 5, 6, 6]
+    ])
+
+    x = x.T
+
+    y = np.array([-1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1])
+    return x, y
 
 
 class MultinomialNB(object):
@@ -40,7 +35,11 @@ class MultinomialNB(object):
         {1: 0.4, 2: 0.6}
         """
         sample_num = len(y) * 1.0
+        if sample_num < 1:
+            raise ValueError
+
         unique_class, class_count = np.unique(y, return_counts=True)
+
         # calculate class prior probability
         for c, num in zip(unique_class, class_count):
             self._dic_class_prior[c] = num / sample_num
@@ -62,24 +61,32 @@ class MultinomialNB(object):
         """
 
         unique_class = np.unique(y)
+
         for c in unique_class:
             self._cd_prob[c] = {}
+
             c_idxs = np.where(y == c)[0]
+
             for i, col_feature in enumerate(x.T):
                 dic_f_prob = {}
                 self._cd_prob[c][i] = dic_f_prob
+
                 for idx in c_idxs:
                     if col_feature[idx] in dic_f_prob:
                         dic_f_prob[col_feature[idx]] += 1
                     else:
                         dic_f_prob[col_feature[idx]] = 1
+
                 for k in dic_f_prob:
                     dic_f_prob[k] = dic_f_prob[k] * 1.0 / len(c_idxs)
 
     def _pred_once(self, x):
         dic_ret = {}
+
         for y in self._dic_class_prior:
             y_prob = self._dic_class_prior[y]
+            print("*************")
+            print(self._cd_prob)
             for i, v in enumerate(x):
                 try:
                     y_prob = y_prob * self._cd_prob[y][i][v]
@@ -96,6 +103,7 @@ class MultinomialNB(object):
             labels = []
             for i in range(x.shape[0]):
                 labels.append(self._pred_once(x[i]))
+
         return labels
 
     def get_class_prior(self):
@@ -105,40 +113,22 @@ class MultinomialNB(object):
         return self._cd_prob
 
 
-if __name__ == "__main__":
-    sample_ratio = 0.1
-    percentage_positive_instances_train = sample_ratio
-    percentage_negative_instances_train = sample_ratio
-
-    percentage_positive_instances_test = sample_ratio
-    percentage_negative_instances_test = sample_ratio
-
-    (pos_train, neg_train, vocab) = load_training_set(percentage_positive_instances_train,
-                                                      percentage_negative_instances_train)
-    (pos_test, neg_test) = load_test_set(percentage_positive_instances_test, percentage_negative_instances_test)
-
-    print("Number of positive training instances:", len(pos_train))
-    print("Number of negative training instances:", len(neg_train))
-    print("Number of positive test instances:", len(pos_test))
-    print("Number of negative test instances:", len(neg_test))
-
-    with open('vocab.txt', 'w', encoding='UTF-8') as f:
-        for word in vocab:
-            f.write("%s\n" % word)
-    print("Vocabulary (training set):", len(vocab))
-
-    vocab = list(vocab)
-    pos_train_vec = [words2vec(vocab, pos_train[index]) for index in range(0, len(pos_train))]
-    pos_train_label = [1] * len(pos_train)
-    neg_train_vec = [words2vec(vocab, neg_train[index]) for index in range(0, len(neg_train))]
-    neg_train_label = [0] * len(neg_train)
-    train_vec = pos_train_vec + neg_train_vec
-    train_label = pos_train_label + neg_train_label
-    x = np.array(train_vec)
-    y = np.array(train_label)
+if __name__ == '__main__':
+    x, y = get_multi_data()
+    print(x.ndim, y.ndim)
+    # 2 1
 
     mnb = MultinomialNB()
     mnb.fit(x, y)
 
-    item = np.array(words2vec(vocab, neg_test[10]))
+    print("class prior probability: %s" % mnb.get_class_prior())
+    # {1: 0.599, -1: 0.40}
+
+    print("feature condition probability: %s" % mnb.get_cd_prob())
+    # { 1: {0: {1: 0.22, 2: 0.33, 3: 0.44}, 1: {4: 0.11, 5: 0.44, 6: 0.44}},
+    #  -1: {0: {1: 0.50, 2: 0.33, 3: 0.16}, 1: {4: 0.50, 5: 0.33, 6: 0.16}}
+    # }
+
+    item = np.array([1, 7])
     print(mnb.predict(item))
+    # {1: 0.02222, -1: 0.06666}
