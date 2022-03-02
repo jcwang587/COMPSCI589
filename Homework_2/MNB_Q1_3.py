38988e0aec6410fce40f90e4c0ec453c0d6c066d
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-03-02 03:11:49 -0500

    update

--- Homework_2/MNB_Q1_3.py
+++ Homework_2/MNB_Q1_3.py
@@ -37,13 +37,13 @@ class NBayes:
 
 
 if __name__ == '__main__':
-    data = np.array([[1, 0, -1], [1, 1, -1], [1, 1, 1], [1, 0, 1],
-                     [1, 0, -1], [2, 0, -1], [2, 1, -1], [2, 1, 1],
-                     [2, 2, 1], [2, 2, 1], [3, 2, 1], [3, 1, 1],
-                     [3, 1, 1], [3, 2, 1], [3, 2, -1]])
+    data = np.array([[1, 0, 1, -1], [1, 1, 1, -1], [1, 1, 1, 1], [1, 0, 1, 1],
+                     [1, 0, 1, -1], [2, 0, 1, -1], [2, 1, 1, -1], [2, 1, --1, 1],
+                     [2, 2, -1, 1], [2, 2, -1, 1], [3, 2,  2, 1], [3, 1, -2, 1],
+                     [3, 1, -1, 1], [3, 2, -1, 1], [3, 2,  4, -1]])
     X_data = data[:, :-1]
     y_data = data[:, -1]
-    clf = NBayes(lambda_=1)
+    clf = NBayes(lambda_=0)
     clf.fit(X_data, y_data)
     print(clf.p_prior, '\n', clf.p_condition)
-    print(clf.predict(np.array([2, 0])))
+    print(clf.predict(np.array([2, 1, 1])))
