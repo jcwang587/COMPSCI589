6a32ceb9c9a96b116838ee16a8df1ec877673e33
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-04-21 20:39:10 -0400

    update

--- Homework_4/bp33.py
+++ Homework_4/bp33.py
@@ -1,43 +1,65 @@
 import numpy as np
 
 
-# 梯度
-def nnGradient(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, Lambda):
+def nn_gradient(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, Lambda):
     length = nn_params.shape[0]
     Theta1 = nn_params[0:hidden_layer_size * (input_layer_size + 1)].reshape(hidden_layer_size,
                                                                              input_layer_size + 1).copy()
-    # 这里使用copy函数，否则下面修改Theta的值，nn_params也会一起修改
     Theta2 = nn_params[hidden_layer_size * (input_layer_size + 1):length].reshape(num_labels,
                                                                                   hidden_layer_size + 1).copy()
     m = X.shape[0]
 
-    '''去掉theta1和theta2的第一列，因为正则化时从1开始'''
     Theta1_colCount = Theta1.shape[1]
     Theta1_x = Theta1[:, 1:Theta1_colCount]
     Theta2_colCount = Theta2.shape[1]
     Theta2_x = Theta2[:, 1:Theta2_colCount]
 
-    '''正向传播，每次需要补上一列1的偏置bias'''
     a1 = np.hstack((np.ones((m, 1)), X))
     z2 = np.dot(a1, np.transpose(Theta1))
     a2 = sigmoid(z2)
     a2 = np.hstack((np.ones((m, 1)), a2))
     z3 = np.dot(a2, np.transpose(Theta2))
-    h = sigmoid(z3)
-    print('a1', a1)
-    print(z2)
-    print(a2)
-    print(z3)
-    print(h)
-    '''反向传播，delta为误差，'''
+    a3 = sigmoid(z3)
+    h = a3
+    term = np.dot(np.transpose(np.vstack((Theta1_x.reshape(-1, 1), Theta2_x.reshape(-1, 1)))),
+                  np.vstack((Theta1_x.reshape(-1, 1), Theta2_x.reshape(-1, 1))))
+    J = -(np.dot(np.transpose(y.reshape(-1, 1)), np.log(h.reshape(-1, 1))) + np.dot(
+        np.transpose(1 - y.reshape(-1, 1)), np.log(1 - h.reshape(-1, 1))) - Lambda * term / 2) / m
+
+    print('Computing the error/cost, J, of the network')
+    print('Processing training instance 1')
+    print('Forward propagating the input [%6.5f]' % X[0, 0])
+    print('a1: [%6.5f %6.5f]' % (a1[0, 0], a1[0, 1]))
+    print('z2: [%6.5f %6.5f]' % (z2[0, 0], z2[0, 1]))
+    print('a2: [%6.5f %6.5f %6.5f]' % (a2[0, 0], a2[0, 1], a2[0, 2]))
+    print('z3: [%6.5f]' % z3[0, 0])
+    print('a3: [%6.5f]' % a3[0, 0])
+    print('f(x): [%6.5f]' % h[0, 0])
+    print('Predicted output for instance 1: [%6.5f]' % h[0, 0])
+    print('Expected output for instance 1: [%6.5f]' % y[0, 0])
+    print('Cost, J, associated with instance 1: [%6.5f]' % J[0, 0])
+
+    print('Processing training instance 2')
+    print('Forward propagating the input [%6.5f]' % X[1, 0])
+    print('a1: [%6.5f %6.5f]' % (a1[1, 0], a1[1, 1]))
+    print('z2: [%6.5f %6.5f]' % (z2[1, 0], z2[1, 1]))
+    print('a2: [%6.5f %6.5f %6.5f]' % (a2[1, 0], a2[1, 1], a2[1, 2]))
+    print('z3: [%6.5f]' % z3[1, 0])
+    print('a3: [%6.5f]' % a3[1, 0])
+    print('f(x): [%6.5f]' % h[1, 0])
+    print('Predicted output for instance 2: [%6.5f]' % h[1, 0])
+    print('Expected output for instance 2: [%6.5f]' % y[1, 0])
+    print('Cost, J, associated with instance 2: [%6.5f]' % J[0, 0])
+    print('Final (regularized) cost, J, based on the complete training set: [%6.5f]' % J)
+
     delta3 = np.zeros((m, num_labels))
     delta2 = np.zeros((m, hidden_layer_size))
     Theta1_grad = []
     Theta2_grad = []
     for i in range(m):
-        Theta1_grad = np.zeros(Theta1.shape)  # 第一层到第二层的权重
-        Theta2_grad = np.zeros(Theta2.shape)  # 第二层到第三层的权重
-        delta3[i, :] = h[i, :] - y[i, :]  # 交叉熵误差率
+        Theta1_grad = np.zeros(Theta1.shape)
+        Theta2_grad = np.zeros(Theta2.shape)
+        delta3[i, :] = h[i, :] - y[i, :]
         Theta2_grad = Theta2_grad + np.dot(np.transpose(delta3[i, :].reshape(1, -1)), a2[i, :].reshape(1, -1))
         delta2[i, :] = np.dot(delta3[i, :].reshape(1, -1), Theta2_x) * sigmoid_gradient(z2[i, :])
         Theta1_grad = Theta1_grad + np.dot(np.transpose(delta2[i, :].reshape(1, -1)), a1[i, :].reshape(1, -1))
@@ -48,20 +70,17 @@ def nnGradient(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y,
 
     Theta1[:, 0] = 0
     Theta2[:, 0] = 0
-    '''梯度'''
     grad = (np.vstack((Theta1_grad.reshape(-1, 1), Theta2_grad.reshape(-1, 1))) +
             Lambda * np.vstack((Theta1.reshape(-1, 1), Theta2.reshape(-1, 1)))) / m
     return np.ravel(grad)
 
 
-# S型函数
 def sigmoid(z):
-    h = np.zeros((len(z), 1))  # 初始化，与z的长度一致
+    h = np.zeros((len(z), 1))
     h = 1.0 / (1.0 + np.exp(-z))
     return h
 
 
-# S型函数导数
 def sigmoid_gradient(z):
     g = sigmoid(z) * (1 - sigmoid(z))
     return g
@@ -75,8 +94,7 @@ if __name__ == "__main__":
     initial_Theta1 = np.array([[0.4, 0.1], [0.3, 0.2]])
     initial_Theta2 = np.array([[0.7, 0.5, 0.6]])
     X = np.array([[0.13], [0.42]])
-    y = np.array([[0.9], [0.23]])  # 初始化y
+    y = np.array([[0.9], [0.23]])
     y = y.reshape(-1, 1)
-    nn_params = np.vstack((initial_Theta1.reshape(-1, 1), initial_Theta2.reshape(-1, 1)))  # 展开theta
-    '''BP求出梯度'''
-    grad = nnGradient(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, Lambda)
+    nn_params = np.vstack((initial_Theta1.reshape(-1, 1), initial_Theta2.reshape(-1, 1)))
+    grad = nn_gradient(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, Lambda)
