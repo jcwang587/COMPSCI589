62298075ca698c6b997c95a15d31b38b2d4a26f1
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-03 03:14:59 -0400

    update

--- Final_Project/1_hand_knn_learning_curve.py
+++ Final_Project/1_hand_knn_learning_curve.py
@@ -83,57 +83,55 @@ if __name__ == '__main__':
     k_fold.append(df9.append(df8.append(df7.append(df6.append(df5.append(df4.append(df3.append(
         df2.append(df1.append(df0))))))))))
 
-    k_list = range(11, 262, 25)
-    final_accuracy = {}
+    k = 11
+    fold_idx = 5
 
-    # Train the k-NN algorithm using training set
-    accuracy = []
-    f1 = []
-    for k in k_list:
-        print('k = ', k)
-        fold_idx = 0
-        accuracy_k = []
-        f1_k = []
-        while fold_idx < 10:
-            print('fold_idx = ', fold_idx)
-            # Split to train and test dataset
-            k_fold_copy = k_fold.copy()
-            data_test = k_fold[fold_idx]
-            del k_fold_copy[fold_idx]
-            data_train = pd.concat(k_fold_copy).sample(n=len(df) - len(data_test.index), replace=True, random_state=587)
-            X_train = data_train.drop(64, axis=1).values
-            y_train = data_train[64].values.astype(int)
-            X_test = data_test.drop(64, axis=1).values
-            y_test = data_test[64].values.astype(int)
+    k_fold_copy = k_fold.copy()
+    data_test = k_fold[fold_idx]
+    del k_fold_copy[fold_idx]
+    data_train = pd.concat(k_fold_copy).sample(n=len(df) - len(data_test.index), replace=True, random_state=587)
+    data_train = data_train.drop(data_train.index[0], axis=0)
 
-            X_train = pd.DataFrame(X_train)
-            X_test = pd.DataFrame(X_test)
-            y_train = pd.Series(y_train.tolist())
-            y_test = pd.Series(y_test.tolist())
+    score = []
+    for n_sample in range(0, len(data_train), 10):
+        # Split to train and test dataset
+        k_fold_copy = k_fold.copy()
+        data_test = k_fold[fold_idx]
+        del k_fold_copy[fold_idx]
+        data_train = pd.concat(k_fold_copy).sample(n=len(df) - len(data_test.index), replace=True, random_state=587)
+        X_train = data_train.drop(64, axis=1).values
+        X_train = np.delete(X_train, range(0, n_sample), axis=0)
+        y_train = data_train[64].values.astype(int)
+        y_train = np.delete(y_train, range(0, n_sample), axis=0)
+        X_test = data_test.drop(64, axis=1).values
+        y_test = data_test[64].values.astype(int)
+        print('Training set size:', len(X_train))
 
-            y_pred = []
-            for index1, row1 in X_test.iterrows():
-                x = row1.values
-                distance = [np.sqrt(np.sum((row2.values - x) ** 2)) for index2, row2 in X_train.iterrows()]
-                idx_sort = np.argsort(distance)
-                y_idx_sort = y_train.values[idx_sort]
-                y_top_k = y_idx_sort[:k]
-                pred = max(set(y_top_k), key=y_top_k.tolist().count)
-                y_pred.append(pred)
-            y_test = y_test.tolist()
-            f1_i = f1_score(y_test, y_pred)
-            accuracy_i = np.sum(np.array(y_pred) == np.array(y_test)) / len(y_test)
-            f1_k.append(f1_i)
-            accuracy_k.append(accuracy_i)
-            fold_idx += 1
-        f1_avg = sum(f1_k) / len(f1_k)
-        accuracy_avg = sum(accuracy_k) / len(accuracy_k)
-        print('accuracy_avg = ', accuracy_avg)
-        print('f1_avg = ', f1_avg)
-        accuracy.append(accuracy_avg)
-        f1.append(f1_avg)
+        X_train = pd.DataFrame(X_train)
+        X_test = pd.DataFrame(X_test)
+        y_train = pd.Series(y_train.tolist())
+        y_test = pd.Series(y_test.tolist())
 
-    plt.plot(k_list, accuracy, '.-', markersize=10, color='#1f77b4')
-    plt.xlabel('Value of k')
-    plt.ylabel('Accuracy over training data')
+        y_pred = []
+        for index1, row1 in X_test.iterrows():
+            x = row1.values
+            distance = [np.sqrt(np.sum((row2.values - x) ** 2)) for index2, row2 in X_train.iterrows()]
+            idx_sort = np.argsort(distance)
+            y_idx_sort = y_train.values[idx_sort]
+            y_top_k = y_idx_sort[:k]
+            pred = max(set(y_top_k), key=y_top_k.tolist().count)
+            y_pred.append(pred)
+        y_test = y_test.tolist()
+        f1_i = f1_score(y_test, y_pred)
+        accuracy_i = np.sum(np.array(y_pred) == np.array(y_test)) / len(y_test)
+        score.append(1-accuracy_i)
+        print('n_sample:', n_sample, 'score:', 1-accuracy_i)
+
+    plt.plot(range(0, len(data_train), 10), sorted(score, reverse=True),
+             '.-', markersize=10, color='#1f77b4')
+    plt.xlabel('Number of training samples')
+    plt.ylabel('Cost function')
+    plt.title('The Hand-Written Dataset')
+    plt.savefig("learning_curve_hand.eps", dpi=600, format="eps")
     plt.show()
+
