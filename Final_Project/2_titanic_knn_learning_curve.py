7dae4a692b333c79da0c3bd08d97ebd92bfac919
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-02 23:53:31 -0400

    update

--- Final_Project/2_titanic_knn_learning_curve.py
+++ Final_Project/2_titanic_knn_learning_curve.py
@@ -45,57 +45,56 @@ if __name__ == '__main__':
         k_fold.append(fold0)
     k_fold.append(df1.append(df0))
 
-    k_list = range(1, 202, 20)
+    k = 41
     final_accuracy = {}
 
     # Train the k-NN algorithm using training set
     accuracy = []
     f1 = []
-    for k in k_list:
-        print('k = ', k)
-        fold_idx = 0
-        accuracy_k = []
-        f1_k = []
-        while fold_idx < 10:
-            print('fold_idx = ', fold_idx)
-            # Split to train and test dataset
-            k_fold_copy = k_fold.copy()
-            data_test = k_fold[fold_idx]
-            del k_fold_copy[fold_idx]
-            data_train = pd.concat(k_fold_copy).sample(n=len(df) - len(data_test.index), replace=True)
-            X_train = data_train.drop('Survived', axis=1).values
-            y_train = data_train['Survived'].values.astype(int)
-            X_test = data_test.drop('Survived', axis=1).values
-            y_test = data_test['Survived'].values.astype(int)
 
-            X_train = pd.DataFrame(X_train)
-            X_test = pd.DataFrame(X_test)
-            y_train = pd.Series(y_train.tolist())
-            y_test = pd.Series(y_test.tolist())
+    fold_idx = 0
+    accuracy_k = []
+    f1_k = []
+    while fold_idx < 10:
+        print('fold_idx = ', fold_idx)
+        # Split to train and test dataset
+        k_fold_copy = k_fold.copy()
+        data_test = k_fold[fold_idx]
+        del k_fold_copy[fold_idx]
+        data_train = pd.concat(k_fold_copy).sample(n=len(df) - len(data_test.index), replace=True)
+        data_train.drop(data_train.columns[0], axis=1)
+        X_train = data_train.drop('Survived', axis=1).values
+        y_train = data_train['Survived'].values.astype(int)
+        X_test = data_test.drop('Survived', axis=1).values
+        y_test = data_test['Survived'].values.astype(int)
 
-            y_pred = []
-            for index1, row1 in X_test.iterrows():
-                x = row1.values
-                distance = [np.sqrt(np.sum((row2.values - x) ** 2)) for index2, row2 in X_train.iterrows()]
-                idx_sort = np.argsort(distance)
-                y_idx_sort = y_train.values[idx_sort]
-                y_top_k = y_idx_sort[:k]
-                pred = max(set(y_top_k), key=y_top_k.tolist().count)
-                y_pred.append(pred)
-            y_test = y_test.tolist()
-            f1_i = f1_score(y_test, y_pred)
-            accuracy_i = np.sum(np.array(y_pred) == np.array(y_test)) / len(y_test)
-            f1_k.append(f1_i)
-            accuracy_k.append(accuracy_i)
-            fold_idx += 1
-        f1_avg = sum(f1_k) / len(f1_k)
-        accuracy_avg = sum(accuracy_k) / len(accuracy_k)
-        print('accuracy_avg = ', accuracy_avg)
-        print('f1_avg = ', f1_avg)
-        accuracy.append(accuracy_avg)
-        f1.append(f1_avg)
+        X_train = pd.DataFrame(X_train)
+        X_test = pd.DataFrame(X_test)
+        y_train = pd.Series(y_train.tolist())
+        y_test = pd.Series(y_test.tolist())
 
-    plt.plot(k_list, accuracy, '.-', markersize=10, color='#1f77b4')
-    plt.xlabel('Value of k')
-    plt.ylabel('Accuracy over training data')
-    plt.show()
+        y_pred = []
+        for index1, row1 in X_test.iterrows():
+            x = row1.values
+            distance = [np.sqrt(np.sum((row2.values - x) ** 2)) for index2, row2 in X_train.iterrows()]
+            idx_sort = np.argsort(distance)
+            y_idx_sort = y_train.values[idx_sort]
+            y_top_k = y_idx_sort[:k]
+            pred = max(set(y_top_k), key=y_top_k.tolist().count)
+            y_pred.append(pred)
+        y_test = y_test.tolist()
+        f1_i = f1_score(y_test, y_pred)
+        accuracy_i = np.sum(np.array(y_pred) == np.array(y_test)) / len(y_test)
+        f1_k.append(f1_i)
+        accuracy_k.append(accuracy_i)
+        fold_idx += 1
+    f1_avg = sum(f1_k) / len(f1_k)
+    accuracy_avg = sum(accuracy_k) / len(accuracy_k)
+    print('accuracy_avg = ', accuracy_avg)
+    print('f1_avg = ', f1_avg)
+
+    #
+    # plt.plot(k_list, accuracy, '.-', markersize=10, color='#1f77b4')
+    # plt.xlabel('Value of k')
+    # plt.ylabel('Accuracy over training data')
+    # plt.show()
